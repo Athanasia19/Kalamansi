f77b056ab4caa88a01e632d01215f50bdee5906d
Athanasia19 <christinejoy.esparas@tup.edu.ph>
2024-02-10 12:29:20 +0800

    Protected route

---
+++ frontend/src/Components/Route/ProtectedRoute.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import { Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import Loader from '../Layout/Loader'
+// import { getUser } from '../../utils/helpers';
+
+const ProtectedRoute = ({ children, isAdmin = false }) => {
+    // const [loading, setLoading] = useState(getUser() === false && false )
+    // const [error, setError] = useState('')
+    // const [user, setUser] = useState(getUser())
+    // const [isAuthenticated, setIsAuthenticated] = useState(false)
+    // console.log(children.type.name, loading)
+    const { isAuthenticated, loading, user } = useSelector(state => state.auth);
+    // if (loading === false) {
+    //     if (!user) {
+    //         return <Navigate to='/login' />
+    //     }
+    //     if (isAdmin === true && user.role !== 'admin') {
+    //         return <Navigate to='/' />
+    //     }
+    //     return children
+    // }
+    if (loading === false) {
+        if (isAuthenticated === false) {
+            return <Navigate to='/login' />
+        }
+        if (isAdmin === true && user.role !== 'admin') {
+            return <Navigate to='/' />
+        }
+        return children
+    }
+    return <Loader />;
+
+};
+
+export default ProtectedRoute;
